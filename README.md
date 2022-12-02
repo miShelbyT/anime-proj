b031c8f54d4c47143d28d37c98ff78fb642e83f4
Shelby Talbert <shelby.talbert@flatironschool.com>
2022-12-02 13:54:15 -0500

    post-lecture code complete plus new API base url

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 **FIS // SCF ANIME PROJECT**
 
-*uses the [GHIBLI API](https://ghibliapi.herokuapp.com/#section/Studio-Ghibli-API)*
+The Ghibli API is no longer up n running on Heroku. Instead we have a much smaller film api that can be found [here](https://anime-server-films.onrender.com) 
 
 To run this application, fork and clone. This project will be using the json-server library for CRUD actions and eventually be deployed via render using an express server.
--- index.js
+++ index.js
@@ -1,3 +1,45 @@
 // welcome to our fun review session where we will be creating an anime project
 
-// we will be using external API: https://ghibliapi.herokuapp.com/#section/Studio-Ghibli-API
+// we will be using a new external API / base URL: https://anime-server-films.onrender.com
+
+
+const header = document.querySelector("header h3")
+// const header = document.getElementById("header")
+// const header = document.querySelectorAll("h3")[2]
+header.innerText = "Anime!!!!!!"
+
+const url = "https://ghibliapi.herokuapp.com"
+
+const renderOneFilm = (movie, i) => {
+  const li = document.createElement("li")
+  li.textContent = `\u{1F3AC} #${i + 1}`;
+  const list = document.querySelector("#list")
+  list.append(li)
+// fetchOneFilm function invocation => arguments real thing
+  li.addEventListener("click", () => fetchOneFilm(movie, li))
+}
+
+//function declaration => parameters (placeholders)
+function fetchOneFilm(film, li) {
+  fetch(film)
+  .then(r => r.json())
+  .then(data => {
+    li.innerText = data.title
+    renderMovieInfo(data)
+  })
+}
+
+function renderMovieInfo(selectedMovie) {
+
+}
+
+
+
+function fetchAnime() {
+  fetch(`${url}/species?name=Human`)
+  .then((r) => r.json())
+  .then(data => data[0].films.forEach((film, i) => renderOneFilm(film, i)))
+}
+
+
+fetchAnime()
